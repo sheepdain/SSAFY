b5938813c36fa71b9c55ff4dc09082beb1dd8225
yangdain <didek5123@naver.com>
2025-03-06 15:13:07 +0900

    algorithm

--- algorithm/min/41/41_3.py
+++ algorithm/min/41/41_3.py
@@ -1,26 +1,18 @@
-# 2000넘어가면 타임에러
-
-def dfs(money, num):
-    global cnt
-
-    if num>cnt:
-        return
-
-    if money == 0:
-        if cnt > num:
-            cnt = num
-        return
-
-    for i in coin:
-        if money >= i:
-            dfs(money - i, num + 1)
-
-
+# 입력 받기
 T, n = map(int, input().split())
 coin = list(map(int, input().split()))
-cnt = 21e8
-dfs(T, 0)
-if cnt==21e8:
-    print('impossible')
+
+# dp 배열 초기화
+dp = [float('inf')] * (T + 1)
+dp[0] = 0  # 0원을 만들기 위해서는 동전이 0개
+
+# 동전의 개수 최소화를 위해 DP 배열 갱신
+for c in coin:
+    for i in range(c, T + 1):
+        dp[i] = min(dp[i], dp[i - c] + 1)
+
+# 결과 출력
+if dp[T] == float('inf'):
+    print("impossible")
 else:
-    print(cnt)
+    print(dp[T])
