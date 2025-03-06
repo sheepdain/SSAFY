35a25be1b3dbf269f05b8a85d9e99e4271ee43be
yangdain <didek5123@naver.com>
2025-03-06 14:44:14 +0900

    algorithm

---
+++ algorithm/min/41/41_3.py
@@ -0,0 +1,24 @@
+def dfs(money, num):
+    global cnt
+
+    if num>cnt:
+        return
+
+    if money == 0:
+        if cnt > num:
+            cnt = num
+        return
+
+    for i in coin:
+        if money >= i:
+            dfs(money - i, num + 1)
+
+
+T, n = map(int, input().split())
+coin = list(map(int, input().split()))
+cnt = 21e8
+dfs(T, 0)
+if cnt==21e8:
+    print('impossible')
+else:
+    print(cnt)
