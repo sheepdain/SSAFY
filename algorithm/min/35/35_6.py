cd88e92003981185a139a7199310d32411af31e5
dain <didek5123@naver.com>
2025-03-12 23:51:07 +0900

    algorithm

---
+++ algorithm/min/35/35_6.py
@@ -0,0 +1,20 @@
+import heapq
+
+n = int(input())
+
+low = [-500]
+high = []
+for i in range(n):
+    a, b = map(int, input().split())
+    for i in [a, b]:
+        if i > -low[0]:
+            heapq.heappush(high, i)
+        else:
+            heapq.heappush(low, -i)
+    if len(low) - 1 > len(high):
+        num = -heapq.heappop(low)
+        heapq.heappush(high, num)
+    else:
+        num = heapq.heappop(high)
+        heapq.heappush(low, -num)
+    print(-low[0])
