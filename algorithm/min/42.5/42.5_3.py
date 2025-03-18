4303b4965a7b1d46f0a2c560f47cf46dd0ea0bae
유영훈 <dbdudgns982@gmail.com>
2025-03-18 17:47:23 +0900

    algorithm

--- algorithm/min/42.5/42.5_3.py
+++ algorithm/min/42.5/42.5_3.py
@@ -1,23 +1,29 @@
-from collections import deque
-
-def one(a, b):
-    return (a - b) * (a + b)
-
-
-def two(a, b):
-    return max(a, b)
-
-
-def three(a, b):
-    return a ** 2 - b ** 2
+def custom_operator(a, b, op):
+    if op == "!!":
+        return (a - b) * (a + b)
+    elif op == "#":
+        return max(a, b)
+    elif op == "$":
+        return a * (2 ** b)
+    elif op == "&":
+        return (a + b) ** 2
 
 
-def fore(a, b):
-    return (a + b) ** 2
+def suhak(idx, value):
+    global cnt
+    if idx == n - 1:
+        if value == 100:
+            cnt += 1
+        return
+
+    for op in operator:
+        new = custom_operator(value, numbers[idx + 1], op)
+        suhak(idx + 1, new)
 
 
-n=int(input())
-arr=list(map(int,input().split()))
-q=deque()
-q.append((arr[0],arr[1]))
-while q:
+n = int(input())
+numbers = list(map(int, input().split()))
+operator = ['!!', '#', '$', '&']
+cnt = 0
+suhak(0, numbers[0])
+print(cnt)
