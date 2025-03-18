d278543cd6655084435f2a81d64b49efc1864072
유영훈 <dbdudgns982@gmail.com>
2025-03-18 15:16:30 +0900

    algorithm

--- algorithm/BAEKJOON/any/12865.py
+++ algorithm/BAEKJOON/any/12865.py
@@ -1,12 +1,10 @@
 n, k = map(int, input().split())
 items = [list(map(int, input().split())) for _ in range(n)]
 
-# 1차원 DP 배열
 dp = [0] * (k + 1)
 
-# 물건을 하나씩 고려
 for weight, value in items:
-    for j in range(k, weight - 1, -1):  # 무게 k부터 weight까지 거꾸로 탐색
+    for j in range(k, weight - 1, -1):
         dp[j] = max(dp[j], dp[j - weight] + value)
 
 print(dp[k])
