aac60d9f76fe30b9d468fec098f12a2a034ea9c5
양다인 <didek5123@naver.com>
2025-04-07 17:47:36 +0900

    algorithm

---
+++ algorithm/BAEKJOON/GROUP/A_training/14888.py
@@ -0,0 +1,36 @@
+def num(level,total):
+    global min_total, max_total, lst
+    if level==n:
+        max_total=max(total, max_total)
+        min_total=min(total, min_total)
+        return
+    
+    for i in range(4):
+        if lst[i]!=0:
+            if i==0:
+                lst[i]-=1
+                num(level+1,total+number[level])
+                lst[i]+=1
+            elif i==1:
+                lst[i]-=1
+                num(level+1,total-number[level])
+                lst[i]+=1
+            elif i==2:
+                lst[i]-=1
+                num(level+1,total*number[level])
+                lst[i]+=1
+            elif i==3:
+                lst[i]-=1
+                num(level+1,total//number[level])
+                lst[i]+=1
+
+
+
+n=int(input())
+number=list(map(int,input().split()))
+lst=list(map(int,input().split()))
+max_total=-10e8
+min_total=10e8
+num(1,number[0])
+print(max_total)
+print(min_total)
