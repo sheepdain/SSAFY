034e39165e4cbee9b254156bcac7a8527f36992f
양다인 <didek5123@naver.com>
2025-04-08 14:36:20 +0900

    db day 2

---
+++ DB_SQL/day2/db_hw_2_2/data.sql
@@ -0,0 +1,34 @@
+-- Active: 1744078797695@@127.0.0.1@3306
+CREATE Table orders(
+  order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  order_date DATE NOT NULL,
+  total_amount REAL NOT NULL
+);
+
+INSERT INTO orders(order_date, total_amount) VALUES
+(2023-07-15,50.99),
+(2023-07-16,75.5),
+(2023-07-17,30.25);
+
+CREATE Table customers(
+  customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  phone TEXT NOT NULL
+);
+
+INSERT INTO customers(name, email, phone) VALUES
+('허균', 'hong.gildong@example.com', '010-1234-5678'),
+('김영희', 'kim.younghee@example.com', '010-9876-5432'),
+('이철수', 'lee.cheolsu@example.com', '010-5555-4444');
+
+DELETE FROM orders
+WHERE order_id=3;
+
+UPDATE customers 
+SET name='홍길동'
+WHERE customer_id=1;
+
+SELECT * FROM orders;
+
+SELECT * FROM customers;
